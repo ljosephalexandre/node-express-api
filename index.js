8f59f722a6756d40318228133591ed17bc359e77
ljosephalexandre <lionel.josephalexandre.dev@gmail.com>
2021-02-11 18:38:13 +0100

    Définition de la route PUT /parkings permettant la modification d'un objet parking dans notre liste d'objet parkings à l'aide de son id

--- index.js
+++ index.js
@@ -28,14 +28,29 @@ app.get('/parkings/:id', (req,res) => {
 });
 
 // Définition de la route POST /parkings
-app.post('/parkings', (req, res) =>{
+app.post('/parkings', (req, res) => {
     // On envoie les données d'un nouveau parking pour les ajouter à notre liste de parkings existante
     parkings.push(req.body);
     // En cas de succès on renvoie la liste d'objet parking actualisée(au format JSON) 
     res.status(200).json(parkings);
 });
 
+// Définition de la route PUT /parkings
+app.put('/parkings/:id', (req, res) => {
+    // On récupère l'id passé en paramètre dans ma route
+    const id = parseInt(req.params.id);
+    // On charge l'objet parking correspondant à l'id passé en paramètre dans l'objet parking
+    let parking = parkings.find(parking => parking.id === id);
+    // On modifie les propriétés de l'objet
+    parking.name = req.name;
+    parking.city = req.body.city;
+    parking.type = req.body.type;
+    // En cas de succès on renvoie l'objet parking mis à jour (au format JSON) 
+    // lors de la consultation de la route paramétrée
+    res.status(200).json(parking);
+})
+
 // Lancement du serveur sur le port d'écoute défini
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Démarrage du serveur 127.0.0.1:${port}`);
 });
