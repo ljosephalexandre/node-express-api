f79d7dc124ba6d6a8cd7f3302513cdfe55260251
ljosephalexandre <lionel.josephalexandre.dev@gmail.com>
2021-02-11 17:59:00 +0100

    Modification du fichier index pour afficher ma liste de parkings à la consultation de la route GET /parkings

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require('express');
 const app = express();
 // Définition du port d'écoute
 const port = process.env.PORT||8080;
+// Chargement du fichier parkings.json dans ma constante parkings
+const parkings = require('./parkings.json');
 
 // Définition de la route GET /parkings
 app.get('/parkings', (req,res) => {
-    res.send('Liste des parkings');
+    res.status(200).json(parkings);
 });
 
 // Lancement du serveur sur le port d'écoute défini
