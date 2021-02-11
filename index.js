8ad27b084e5b2f4177852209f073f71bff0ca71a
ljosephalexandre <lionel.josephalexandre.dev@gmail.com>
2021-02-11 18:08:31 +0100

    Création de la route GET /parkings/:id

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get('/parkings', (req,res) => {
     res.status(200).json(parkings);
 });
 
+//Définition de la route GET /parkings/:id
+app.get('/parkings/:id', (req,res) => {
+    // On récupère l'id passé en paramètre dans ma route
+    const id = parseInt(req.params.id);
+    // On charge l'objet parking correspondant à l'id passé en paramètre dans l'objet parking
+    const parking = parkings.find(parking => parking.id === id);
+    // En cas de succès on renvoie l'objet parking (au format JSON) 
+    // lors de la consultation de la route paramétrée
+    res.status(200).json(parking);
+});
+
 // Lancement du serveur sur le port d'écoute défini
 app.listen(port, () =>{
     console.log(`Démarrage du serveur 127.0.0.1:${port}`);
